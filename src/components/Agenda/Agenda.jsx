0c94679ffc2042452456114b8da7b0d4b895dabe
Y2Kuwu <jamesmfox5@gmail.com>
2023-03-13 18:02:41 -0700

    agenda component

---
+++ src/components/Agenda/Agenda.jsx
@@ -0,0 +1,28 @@
+import { Component } from "react";
+
+
+export default class Agenda extends Component{
+    constructor(props){
+    super(props)
+    this.state =
+    {
+      
+    
+    }
+        this.getDateList = this.getDateList.bind(this, false);  //send id / date to check schdeduled events 
+        
+    
+    }
+    getDateList()
+    {
+
+    }
+
+    render(){
+    return(
+        <>
+
+        </>
+    )
+    }
+    }
